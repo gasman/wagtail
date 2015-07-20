620d27772ce4921eda2bc0ef7c0c1444f22db548
Matt Westcott <matt@west.co.tt>
2015-07-20 17:43:19 +0100

    Add Collection model/migrations

---
+++ wagtail/wagtailcore/migrations/0017_collection.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('wagtailcore', '0001_squashed_0016_change_page_url_path_to_text_field'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Collection',
+            fields=[
+                ('id', models.AutoField(serialize=False, auto_created=True, verbose_name='ID', primary_key=True)),
+                ('name', models.CharField(max_length=255, verbose_name='Name')),
+            ],
+            options={
+                'verbose_name': 'Collection',
+            },
+            bases=(models.Model,),
+        ),
+    ]
