0be47e30b939ceb6b387e28c96422f3d677959bc
Matt Westcott <matt@west.co.tt>
2015-07-30 15:58:27 +0100

    Convert collections admin views to class-based views

--- wagtail/wagtailadmin/urls/collections.py
+++ wagtail/wagtailadmin/urls/collections.py
@@ -4,8 +4,8 @@ from wagtail.wagtailadmin.views import collections
 
 
 urlpatterns = [
-    url(r'^$', collections.index, name='index'),
-    url(r'^add/$', collections.create, name='add'),
-    url(r'^(\d+)/$', collections.edit, name='edit'),
-    url(r'^(\d+)/delete/$', collections.delete, name='delete'),
+    url(r'^$', collections.Index.as_view(), name='index'),
+    url(r'^add/$', collections.Create.as_view(), name='add'),
+    url(r'^(\d+)/$', collections.Edit.as_view(), name='edit'),
+    url(r'^(\d+)/delete/$', collections.Delete.as_view(), name='delete'),
 ]
--- wagtail/wagtailadmin/views/collections.py
+++ wagtail/wagtailadmin/views/collections.py
@@ -1,74 +1,91 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import permission_required
 from django.core.urlresolvers import reverse
+from django.utils.decorators import method_decorator
 from django.utils.translation import ugettext as _
+from django.views.generic.base import View
 
 from wagtail.wagtailadmin.forms import CollectionForm
 from wagtail.wagtailadmin import messages
 from wagtail.wagtailcore.models import Collection
 
 
-@permission_required('wagtailcore.change_collection')
-def index(request):
-    collections = Collection.objects.order_by('name')
+class Index(View):
+    @method_decorator(permission_required('wagtailcore.change_collection'))
+    def get(self, request):
+        collections = Collection.objects.order_by('name')
 
-    return render(request, "wagtailadmin/collections/index.html", {
-        'collections': collections,
-    })
+        return render(request, "wagtailadmin/collections/index.html", {
+            'collections': collections,
+        })
 
 
-@permission_required('wagtailcore.add_collection')
-def create(request):
-    if request.method == 'POST':
-        form = CollectionForm(request.POST)
-        if form.is_valid():
-            collection = form.save()
+class Create(View):
+    @method_decorator(permission_required('wagtailcore.add_collection'))
+    def get(self, request):
+        self.form = CollectionForm()
+        return self.render_to_response()
+
+    @method_decorator(permission_required('wagtailcore.add_collection'))
+    def post(self, request):
+        self.form = CollectionForm(request.POST)
+        if self.form.is_valid():
+            collection = self.form.save()
             messages.success(request, _("Collection '{0}' created.").format(collection), buttons=[
                 messages.button(reverse('wagtailadmin_collections:edit', args=(collection.id,)), _('Edit'))
             ])
             return redirect('wagtailadmin_collections:index')
         else:
             messages.error(request, _("The collection could not be created due to errors."))
-    else:
-        form = CollectionForm()
+            return self.render_to_response()
 
-    return render(request, 'wagtailadmin/collections/create.html', {
-        'form': form,
-    })
+    def render_to_response(self):
+        return render(self.request, 'wagtailadmin/collections/create.html', {
+            'form': self.form,
+        })
 
 
-@permission_required('wagtailcore.change_collection')
-def edit(request, collection_id):
-    collection = get_object_or_404(Collection, id=collection_id)
+class Edit(View):
+    @method_decorator(permission_required('wagtailcore.change_collection'))
+    def get(self, request, collection_id):
+        self.collection = get_object_or_404(Collection, id=collection_id)
+        self.form = CollectionForm(instance=self.collection)
+        return self.render_to_response()
 
-    if request.POST:
-        form = CollectionForm(request.POST, instance=collection)
-        if form.is_valid():
-            collection = form.save()
-            messages.success(request, _("Collection '{0}' updated.").format(collection), buttons=[
-                messages.button(reverse('wagtailadmin_collections:edit', args=(collection.id,)), _('Edit'))
+    @method_decorator(permission_required('wagtailcore.change_collection'))
+    def post(self, request, collection_id):
+        self.collection = get_object_or_404(Collection, id=collection_id)
+
+        self.form = CollectionForm(request.POST, instance=self.collection)
+        if self.form.is_valid():
+            self.form.save()
+            messages.success(request, _("Collection '{0}' updated.").format(self.collection), buttons=[
+                messages.button(reverse('wagtailadmin_collections:edit', args=(self.collection.id,)), _('Edit'))
             ])
             return redirect('wagtailadmin_collections:index')
         else:
             messages.error(request, _("The collection could not be saved due to errors."))
-    else:
-        form = CollectionForm(instance=collection)
+            return self.render_to_response()
 
-    return render(request, 'wagtailadmin/collections/edit.html', {
-        'collection': collection,
-        'form': form,
-    })
+    def render_to_response(self):
+        return render(self.request, 'wagtailadmin/collections/edit.html', {
+            'collection': self.collection,
+            'form': self.form,
+        })
 
 
-@permission_required('wagtailcore.delete_collection')
-def delete(request, collection_id):
-    collection = get_object_or_404(Collection, id=collection_id)
+class Delete(View):
+    @method_decorator(permission_required('wagtailcore.delete_collection'))
+    def get(self, request, collection_id):
+        collection = get_object_or_404(Collection, id=collection_id)
+        return render(request, "wagtailadmin/collections/confirm_delete.html", {
+            'collection': collection,
+        })
 
-    if request.POST:
+    @method_decorator(permission_required('wagtailcore.delete_collection'))
+    def post(self, request, collection_id):
+        collection = get_object_or_404(Collection, id=collection_id)
+
         collection.delete()
         messages.success(request, _("Collection '{0}' deleted.").format(collection))
         return redirect('wagtailadmin_collections:index')
-
-    return render(request, "wagtailadmin/collections/confirm_delete.html", {
-        'collection': collection,
-    })
