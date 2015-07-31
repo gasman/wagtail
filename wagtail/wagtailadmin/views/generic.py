c95f624656921a1f6a4caa8169ef6fa4768ccc17
Matt Westcott <matt@west.co.tt>
2015-07-31 15:18:31 +0100

    make 'edit collection' template fully generic

--- wagtail/wagtailadmin/views/generic.py
+++ wagtail/wagtailadmin/views/generic.py
@@ -1,6 +1,6 @@
 from django.core.urlresolvers import reverse
 from django.shortcuts import render, redirect, get_object_or_404
-from django.utils.translation import ugettext as _
+from django.utils.translation import ugettext_lazy, ugettext as _
 from django.views.generic.base import View
 
 from wagtail.wagtailadmin import messages
@@ -68,11 +68,25 @@ class CreateView(PermissionCheckedView):
 
 
 class EditView(PermissionCheckedView):
+    template = 'wagtailadmin/generic/edit.html'
+    form_template = 'wagtailadmin/generic/_form.html'
+
+    page_title = ugettext_lazy("Editing")
+
+    def get_page_subtitle(self):
+        return str(self.instance)
+
     def get_success_message(self, instance):
         return self.success_message.format(instance)
 
     def get_error_message(self):
         return self.error_message
+
+    def get_edit_url(self):
+        return reverse(self.edit_url_name, args=(self.instance.id,))
+
+    def get_delete_url(self):
+        return reverse(self.delete_url_name, args=(self.instance.id,))
 
     def get(self, request, instance_id):
         self.instance = get_object_or_404(self.model, id=instance_id)
@@ -86,7 +100,7 @@ class EditView(PermissionCheckedView):
         if self.form.is_valid():
             self.form.save()
             messages.success(request, self.get_success_message(self.instance), buttons=[
-                messages.button(reverse(self.edit_url_name, args=(self.instance.id,)), _('Edit'))
+                messages.button(self.get_edit_url(), _('Edit'))
             ])
             return redirect(self.index_url_name)
         else:
@@ -95,8 +109,10 @@ class EditView(PermissionCheckedView):
 
     def render_to_response(self):
         return render(self.request, self.template, {
+            'view': self,
             self.context_object_name: self.instance,
             'form': self.form,
+            'can_delete': self.request.user.has_perm(self.delete_permission_name),
         })
 
 
