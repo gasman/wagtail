263e29021232709f4dafdbf469e8c080c65da061
Matt Westcott <matt@west.co.tt>
2015-11-07 12:11:20 +0100

    Add merge migration to wagtailcore.
    Don't renumber the collections migrations (at least for now) so that USW can migrate from their custom build

---
+++ wagtail/wagtailcore/migrations/0023_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('wagtailcore', '0022_group_collection_permission'),
+        ('wagtailcore', '0020_add_index_on_page_first_published_at'),
+    ]
+
+    operations = [
+    ]
