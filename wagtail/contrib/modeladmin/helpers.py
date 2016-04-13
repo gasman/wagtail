fe3c4a9ff74b0272650bdbe168ea5c3471c45022
Karl Hobley <karlhobley10@gmail.com>
2016-04-13 17:04:19 +0100

    Fixed permission name

--- wagtail/contrib/modeladmin/helpers.py
+++ wagtail/contrib/modeladmin/helpers.py
@@ -181,7 +181,7 @@ class PagePermissionHelper(PermissionHelper):
             pages_where_user_can_add = Page.objects.none()
             user_perms = UserPagePermissionsProxy(user)
 
-            for perm in user_perms.permissions.filter(permission_type='publish'):
+            for perm in user_perms.permissions.filter(permission_type='add'):
                 # user has add permission on any subpage of perm.page
                 # (including perm.page itself)
                 pages_where_user_can_add |= Page.objects.descendant_of(perm.page, inclusive=True)
