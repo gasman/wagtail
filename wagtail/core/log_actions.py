8f1e4b900726da9819cbf90879ec2ff712b27a49
Matt Westcott <matt@west.co.tt>
2021-06-16 14:50:51 +0100

    Revert "Allow different registries to be created for different object types"
    This reverts commit f2f6167f5d0516a41d33ad73433f469b93dc77b5.
    
    Rather than having a separate registry per object type, it makes sense to have a single registry to keep track of how all object types handle logging. Otherwise, we'll end up needing a registry of registries to see which registry we should consult for a given model...

--- wagtail/core/log_actions.py
+++ wagtail/core/log_actions.py
@@ -8,10 +8,8 @@ class LogActionRegistry:
     A central store for log actions.
     The expected format for registered log actions: Namespaced action, Action label, Action message (or callable)
     """
-    def __init__(self, hook_name):
-        self.hook_name = hook_name
-
-        # Has the hook been run for this registry?
+    def __init__(self):
+        # Has the register_log_actions hook been run for this registry?
         self.has_scanned_for_actions = False
 
         # Holds the actions.
@@ -28,7 +26,7 @@ class LogActionRegistry:
 
     def scan_for_actions(self):
         if not self.has_scanned_for_actions:
-            for fn in hooks.get_hooks(self.hook_name):
+            for fn in hooks.get_hooks('register_log_actions'):
                 fn(self)
 
             self.has_scanned_for_actions = True
@@ -83,5 +81,4 @@ class LogActionRegistry:
         return self.get_actions()[action][0]
 
 
-# For historical reasons, pages use the 'register_log_actions' hook
-page_log_action_registry = LogActionRegistry('register_log_actions')
+page_log_action_registry = LogActionRegistry()
