9f320e5a582cba1ca4011b2ce333127820f1639c
Matt Westcott <matt@west.co.tt>
2021-09-08 15:15:45 +0100

    Retrieve instances with log entries and use AdminURLFinder to locate edit URLs

--- wagtail/core/models/__init__.py
+++ wagtail/core/models/__init__.py
@@ -3984,9 +3984,6 @@ class PageLogEntryQuerySet(LogEntryQuerySet):
         else:
             return self.none()
 
-    def prefetch_instances(self):
-        return self.prefetch_related('page')
-
 
 class PageLogEntryManager(BaseLogEntryManager):
     def get_queryset(self):
